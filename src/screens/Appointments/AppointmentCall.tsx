6293cbd218b6401c19a5075f7112db93a49c4a78
khoi lam <khoilam.dev@gmail.com>
2023-09-09 11:52:35 +0700

    update model doctor

--- src/screens/Appointments/AppointmentCall.tsx
+++ src/screens/Appointments/AppointmentCall.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
 
 import {
+    Image,
     StyleSheet,
-    Text,
     View,
 } from 'react-native';
+
+import background from '../../assets/images/video-call-image.png';
 
 const AppointmentCall = (): JSX.Element => {
 
     return (
         <View>
-            <Text style={[styles.container]}>
-                AppointmentCall
-            </Text >
+            <Image source={background} style={styles.background} />
         </View>
 
     );
@@ -22,5 +22,10 @@ export default AppointmentCall;
 
 const styles = StyleSheet.create({
     container: {
+    },
+    background: {
+        alignSelf: 'center',
+        // resizeMode: 'cover',
+        // height: 40,a
     },
 })
