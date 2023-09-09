eb5e1fd586ef941df7ab4753b1f8f4a5fa3de32b
khoi lam <khoilam.dev@gmail.com>
2023-09-09 22:52:25 +0700

    feat: doctor detail sreen

--- src/navigations/AppNavigator.tsx
+++ src/navigations/AppNavigator.tsx
@@ -1,105 +1,114 @@
 import React from 'react';
 
-import {Image} from 'react-native';
+import { Image } from 'react-native';
 
-import {createStackNavigator} from '@react-navigation/stack';
+import { createStackNavigator } from '@react-navigation/stack';
 
 import avartar from '../assets/images/mathew.jpg';
 import AppointmentBooking from '../screens/Appointments/AppointmentBooking';
 import AppointmentCall from '../screens/Appointments/AppointmentCall';
 import AppointmentChat from '../screens/Appointments/AppointmentChat';
 import Appointments from '../screens/Appointments/Appointments';
+import DoctorDetail from '../screens/Doctors/DoctorDetail';
 import Doctors from '../screens/Doctors/Doctors';
 import Home from '../screens/Home/Home';
 import MedicationDetails from '../screens/Medications/MedicationDetails';
 import Medications from '../screens/Medications/Medications';
 import Profile from '../screens/Profile/Profile';
 import Welcome from '../screens/Welcome';
-import {spacing, theme} from '../theme';
+import {
+    spacing,
+    theme,
+} from '../theme';
 
 const Stack = createStackNavigator();
 
 const AppNavigator = (): JSX.Element => {
-  return (
-    <Stack.Navigator>
-      <Stack.Screen
-        name="welcome"
-        options={{headerShown: false}}
-        component={Welcome}
-      />
-      <Stack.Screen
-        name="profile"
-        options={{headerShown: false}}
-        component={Profile}
-      />
-      <Stack.Screen
-        name="home"
-        options={{headerShown: false}}
-        component={Home}
-      />
-      <Stack.Screen name="medications" component={Medications} />
-      <Stack.Screen
-        name="appointments"
-        options={{
-          title: 'Your appointments',
-          headerStyle: {backgroundColor: theme.standardBackground},
-          headerRight: () =>
-            (
-              <Image
-                style={{
-                  height: 30,
-                  width: 30,
-                  borderRadius: 50,
-                  margin: spacing.s,
-                  resizeMode: 'cover',
+    return (
+        <Stack.Navigator>
+            <Stack.Screen
+                name="welcome"
+                options={{ headerShown: false }}
+                component={Welcome}
+            />
+            <Stack.Screen
+                name="profile"
+                options={{ headerShown: false }}
+                component={Profile}
+            />
+            <Stack.Screen
+                name="home"
+                options={{ headerShown: false }}
+                component={Home}
+            />
+            <Stack.Screen name="medications" component={Medications} />
+            <Stack.Screen
+                name="appointments"
+                options={{
+                    title: 'Your appointments',
+                    headerStyle: { backgroundColor: theme.standardBackground },
+                    headerRight: () =>
+                        (
+                            <Image
+                                style={{
+                                    height: 30,
+                                    width: 30,
+                                    borderRadius: 50,
+                                    margin: spacing.s,
+                                    resizeMode: 'cover',
+                                }}
+                                resizeMethod="auto"
+                                source={avartar}
+                            />
+                        ) as React.ReactElement,
                 }}
-                resizeMethod="auto"
-                source={avartar}
-              />
-            ) as React.ReactElement,
-        }}
-        component={Appointments}
-      />
-      <Stack.Screen
-        options={{title: '', headerTransparent: true}}
-        name="appointmentChat"
-        component={AppointmentChat}
-      />
-      <Stack.Screen
-        name="appointmentCall"
-        component={AppointmentCall}
-        options={{title: '', headerTransparent: true}}
-      />
-      <Stack.Screen
-        name="doctors"
-        component={Doctors}
-        options={{title: '', headerTransparent: true}}
-      />
-      <Stack.Screen name="medicationDetails" component={MedicationDetails} />
-      <Stack.Screen
-        name="appointmentBooking"
-        options={{
-          title: 'Book your appointment',
-          headerStyle: {backgroundColor: theme.standardBackground},
-          headerRight: () =>
-            (
-              <Image
-                style={{
-                  height: 30,
-                  width: 30,
-                  borderRadius: 50,
-                  margin: spacing.s,
-                  resizeMode: 'cover',
+                component={Appointments}
+            />
+            <Stack.Screen
+                options={{ title: '', headerTransparent: true }}
+                name="appointmentChat"
+                component={AppointmentChat}
+            />
+            <Stack.Screen
+                name="appointmentCall"
+                component={AppointmentCall}
+                options={{ title: '', headerTransparent: true }}
+            />
+            <Stack.Screen
+                name="doctors"
+                component={Doctors}
+                options={{ title: '', headerTransparent: true }}
+            />
+            <Stack.Screen
+                name="doctorDetail"
+                component={DoctorDetail}
+                options={{ title: '', headerTransparent: true }}
+            />
+            <Stack.Screen name="medicationDetails" component={MedicationDetails} />
+            <Stack.Screen
+                name="appointmentBooking"
+                options={{
+                    title: 'Book your appointment',
+                    headerStyle: { backgroundColor: theme.standardBackground },
+                    headerRight: () =>
+                        (
+                            <Image
+                                style={{
+                                    height: 30,
+                                    width: 30,
+                                    borderRadius: 50,
+                                    margin: spacing.s,
+                                    resizeMode: 'cover',
+                                }}
+                                resizeMethod="auto"
+                                source={avartar}
+                            />
+                        ) as React.ReactElement,
                 }}
-                resizeMethod="auto"
-                source={avartar}
-              />
-            ) as React.ReactElement,
-        }}
-        component={AppointmentBooking}
-      />
-    </Stack.Navigator>
-  );
+                component={AppointmentBooking}
+            />
+        </Stack.Navigator>
+    );
 };
 
 export default AppNavigator;
